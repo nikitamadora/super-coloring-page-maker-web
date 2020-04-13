71d36d01c29dd90a8eb21b6ce78092940de6e208
nmadora <nikitamadora@gmail.com>
2020-04-12 20:29:27 -0700

    Adds button that makes initial connection to back-end

--- _javascript/main.js
+++ _javascript/main.js
@@ -1,3 +1,31 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('Hello Bulma!');
 });
+
+const button = document.querySelector("#test-button");
+const endpoint = 'http://localhost:4000/api/v1/artboard'
+
+// const index = (req, res) => {
+//   return fetch(endpoint, {
+//     method: 'GET',
+//     redirect: 'follow'
+//   })
+//   .then((res) => {
+//     console.log(res);
+//     return res.json();
+//   })
+//   // .then(console.log('res', res))
+//   .catch(err => console.log(err));
+// };
+
+const requestOptions = {
+  method: 'GET',
+  redirect: 'follow'
+};
+
+button.addEventListener('click', () => {
+  fetch("http://localhost:4000/api/v1/artboard", requestOptions)
+    .then(response => response.json())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
+});
--- dist/lib/main.js
+++ dist/lib/main.js
@@ -3,3 +3,34 @@
 document.addEventListener('DOMContentLoaded', function () {
   console.log('Hello Bulma!');
 });
+
+var button = document.querySelector("#test-button");
+var endpoint = 'http://localhost:4000/api/v1/artboard';
+
+// const index = (req, res) => {
+//   return fetch(endpoint, {
+//     method: 'GET',
+//     redirect: 'follow'
+//   })
+//   .then((res) => {
+//     console.log(res);
+//     return res.json();
+//   })
+//   // .then(console.log('res', res))
+//   .catch(err => console.log(err));
+// };
+
+var requestOptions = {
+  method: 'GET',
+  redirect: 'follow'
+};
+
+button.addEventListener('click', function () {
+  fetch("http://localhost:4000/api/v1/artboard", requestOptions).then(function (response) {
+    return response.json();
+  }).then(function (result) {
+    return console.log(result);
+  }).catch(function (error) {
+    return console.log('error', error);
+  });
+});
