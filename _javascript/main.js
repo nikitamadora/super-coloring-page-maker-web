2c6f8971700df408dcf9850a5271d3b86683fd53
nmadora <nikitamadora@gmail.com>
2020-04-13 17:42:22 -0700

    Adds random math functionality

--- _javascript/main.js
+++ _javascript/main.js
@@ -1,31 +1,64 @@
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('Hello Bulma!');
+  console.log('Hello Developer! Check out my GitHub, and feel free to play!');
 });
 
-const button = document.querySelector("#test-button");
-const endpoint = 'http://localhost:4000/api/v1/artboard'
+// ----------------------------------------------------
+//                    Canvas Logic
+// ----------------------------------------------------
+const canvasForm = document.querySelector('#canvas-form');
+let numShapes = document.querySelector('#num-shapes');
 
-// const index = (req, res) => {
-//   return fetch(endpoint, {
-//     method: 'GET',
-//     redirect: 'follow'
-//   })
-//   .then((res) => {
-//     console.log(res);
-//     return res.json();
-//   })
-//   // .then(console.log('res', res))
-//   .catch(err => console.log(err));
-// };
+let getRandomNum = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+};
+
+const createCanvas = (e) => {
+  e.preventDefault();
+  console.log('Creating shapes!');
+  const canvas = [];
+  numShapes = numShapes.value;
+  for (let i = 0; i < numShapes; i++) {
+    let shape = {
+      id: 0,
+      x: 0,
+      y: 0,
+      h: 0,
+      w: 0
+    }
+    shape.id = i;
+    shape.x = getRandomNum(0, 300);
+    shape.y = getRandomNum(0, 300);
+    shape.h = getRandomNum(3, 250);
+    shape.w = getRandomNum(3, 250);
+    console.log(shape);
+  }
+  console.log(canvas);
+};
+
+// -------------------------------------- Event Listeners
+canvasForm.addEventListener('submit', createCanvas);
+
+
+
+
+
+// ------------------------------------------------- API Requests
+
+// const recreateButton = document.querySelector("#recreate-btn");
+const endpoint = 'http://localhost:4000/api/v1/artboard'
 
 const requestOptions = {
   method: 'GET',
   redirect: 'follow'
 };
 
-button.addEventListener('click', () => {
-  fetch("http://localhost:4000/api/v1/artboard", requestOptions)
-    .then(response => response.json())
-    .then(result => console.log(result))
-    .catch(error => console.log('error', error));
-});
+// Fetches all artboard data
+// recreateButton.addEventListener('click', () => {
+//   fetch("http://localhost:4000/api/v1/artboard", requestOptions)
+//     .then(response => response.json())
+//     .then(result => console.log(result))
+//     .catch(error => console.log('error', error));
+// });
+
--- dist/lib/main.js
+++ dist/lib/main.js
@@ -1,36 +1,61 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', function () {
-  console.log('Hello Bulma!');
+  console.log('Hello Developer! Check out my GitHub, and feel free to play!');
 });
 
-var button = document.querySelector("#test-button");
-var endpoint = 'http://localhost:4000/api/v1/artboard';
+// ----------------------------------------------------
+//                    Canvas Logic
+// ----------------------------------------------------
+var canvasForm = document.querySelector('#canvas-form');
+var numShapes = document.querySelector('#num-shapes');
 
-// const index = (req, res) => {
-//   return fetch(endpoint, {
-//     method: 'GET',
-//     redirect: 'follow'
-//   })
-//   .then((res) => {
-//     console.log(res);
-//     return res.json();
-//   })
-//   // .then(console.log('res', res))
-//   .catch(err => console.log(err));
-// };
+var getRandomNum = function getRandomNum(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+};
+
+var createCanvas = function createCanvas(e) {
+  e.preventDefault();
+  console.log('Creating shapes!');
+  var canvas = [];
+  numShapes = numShapes.value;
+  for (var i = 0; i < numShapes; i++) {
+    var shape = {
+      id: 0,
+      x: 0,
+      y: 0,
+      h: 0,
+      w: 0
+    };
+    shape.id = i;
+    shape.x = getRandomNum(0, 300);
+    shape.y = getRandomNum(0, 300);
+    shape.h = getRandomNum(3, 250);
+    shape.w = getRandomNum(3, 250);
+    console.log(shape);
+  }
+  console.log(canvas);
+};
+
+// -------------------------------------- Event Listeners
+canvasForm.addEventListener('submit', createCanvas);
+
+// ------------------------------------------------- API Requests
+
+// const recreateButton = document.querySelector("#recreate-btn");
+var endpoint = 'http://localhost:4000/api/v1/artboard';
 
 var requestOptions = {
   method: 'GET',
   redirect: 'follow'
 };
 
-button.addEventListener('click', function () {
-  fetch("http://localhost:4000/api/v1/artboard", requestOptions).then(function (response) {
-    return response.json();
-  }).then(function (result) {
-    return console.log(result);
-  }).catch(function (error) {
-    return console.log('error', error);
-  });
-});
+// Fetches all artboard data
+// recreateButton.addEventListener('click', () => {
+//   fetch("http://localhost:4000/api/v1/artboard", requestOptions)
+//     .then(response => response.json())
+//     .then(result => console.log(result))
+//     .catch(error => console.log('error', error));
+// });
