2fcd9de55d047168731f6ae34ce621f55d7f8bb7
nmadora <nikitamadora@gmail.com>
2020-04-14 21:34:54 -0700

    Adds recreateCanvas logic for single artboard view

--- _javascript/view.js
+++ _javascript/view.js
@@ -1,19 +1,41 @@
 document.addEventListener('DOMContentLoaded', () => {
   const API_BASE = '/api/v1';
-  // const artboardId = window.location.pathname.split('/')[1];
+  const artboardId = window.location.pathname.split('/')[2];
   // console.log(artboardId);
   const canvas = document.querySelector('.canvas');
-  const canvasData = [];
-  let numShapes = document.querySelector('#num-shapes');
 
-  const recreateCanvas = () => {
+  const fetchData = () => {
     console.log('Rebuilding Canvas!');
     // Send GET request, including ID of artboard
-    fetch(`${API_BASE}/artboard/${artboardId}`)
-      .then(stream => stream.json())
-      .then(result => console.log(result))
+    fetch(`http://localhost:4000${API_BASE}/artboard/${artboardId}`)
+      .then((stream) => stream.json())
+      .then(res => recreateCanvas(res))
       .catch(error => console.log('error', error));
   }
+
+  fetchData();
+
+  const recreateCanvas = (canvasObj) => {
+    // const shapeTemplate = getShapeTemplate(canvasObj);
+    let importedData = JSON.parse(canvasObj.canvasData);
+    let numShapes = importedData.length;
+    
+    for (let i = 0; i < numShapes; i++) {
+      let shape = {
+        id: i,
+        x: importedData[i].x,
+        y: importedData[i].y,
+        h: importedData[i].h,
+        w: importedData[i].w
+      }
+      // canvasData.push(shape);
+      let domShape = document.createElement('div');
+      domShape.setAttribute('class', 'rect');
+      domShape.setAttribute('style', `width:${shape.w}px; height:${shape.h}px; transform: translate(${shape.x}px,${shape.y}px`);
+      canvas.appendChild(domShape);
+    }
+  };
+
     // const canvas = [];
     // numShapes = numShapes.value;
     // for (let i = 0; i < numShapes; i++) {
